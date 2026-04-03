a63c9effb01d185890085a837a2eb4e8eb1430b0
unknown <wilkickitomas-ops>
2026-04-03 21:12:56 +0300

    pridieta pazimio keitima ir mokiniu trinima

--- 2 praktine uzduotis/main.cpp
+++ 2 praktine uzduotis/main.cpp
@@ -18,6 +18,8 @@ int main() {
         cout << "1 - Naujas mokinys\n";
         cout << "2 - Rodyti visus\n";
         cout << "3 - Rodyti viena\n";
+        cout << "4 - Keisti pazymi\n";
+        cout << "5 - Trinti mokini\n";
         cout << "0 - Iseiti\n";
         cout << ">>> ";
         cin >> pasirinkimas;
@@ -77,6 +79,72 @@ int main() {
                     }
                     cout << endl;
                     rasta = true;
+                }
+            }
+
+            if (!rasta) {
+                cout << "Nerasta.\n";
+            }
+            break;
+        }
+
+        case 4: {
+            string v;
+            bool rasta = false;
+
+            cout << "Vardas: ";
+            cin >> v;
+
+            for (int i = 0; i < mokKiekis; i++) {
+                if (vardai[i] == v) {
+
+                    for (int j = 0; j < kiekPaz[i]; j++) {
+                        cout << j + 1 << ":" << pazymiai[i][j] << " ";
+                    }
+                    cout << endl;
+
+                    int nr, naujas;
+                    cout << "Kurio nr keisti: ";
+                    cin >> nr;
+
+                    cout << "Naujas pazymys: ";
+                    cin >> naujas;
+
+                    pazymiai[i][nr - 1] = naujas;
+                    cout << "Pakeista!\n";
+                    rasta = true;
+                }
+            }
+
+            if (!rasta) {
+                cout << "Nerasta.\n";
+            }
+            break;
+        }
+
+        case 5: {
+            string v;
+            bool rasta = false;
+
+            cout << "Kuri trinti: ";
+            cin >> v;
+
+            for (int i = 0; i < mokKiekis; i++) {
+                if (vardai[i] == v) {
+
+                    for (int k = i; k < mokKiekis - 1; k++) {
+                        vardai[k] = vardai[k + 1];
+                        kiekPaz[k] = kiekPaz[k + 1];
+
+                        for (int j = 0; j < MAX_PAZ; j++) {
+                            pazymiai[k][j] = pazymiai[k + 1][j];
+                        }
+                    }
+
+                    mokKiekis--;
+                    cout << "Istrinta!\n";
+                    rasta = true;
+                    break;
                 }
             }
 
