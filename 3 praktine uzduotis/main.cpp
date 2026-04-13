c6714fef88297156db82e2f104c821743e82167c
Tomas VIKO <wilkickitomas@gmail.com>
2026-04-13 12:05:00 +0300

    3 praktine užduotis

---
+++ 3 praktine uzduotis/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+const int MAX_ITEMS = 100;
+const double VAT_RATE = 0.21;
+
+struct menuItemType {
+    string menuItem;   // patiekalo pavadinimas
+    double menuPrice;  // patiekalo kaina
+};
+
+int main() {
+    cout << "3 praktine užduotis" << endl;
+    return 0;
+}
