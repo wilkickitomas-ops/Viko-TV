8381f8459e7b0303cc88730529786a86a8b0e70a
Tomas VIKO <wilkickitomas@gmail.com>
2026-06-02 12:00:35 +0300

    Suliejimo sortiravimas...

--- Kursinis/main.cpp
+++ Kursinis/main.cpp
@@ -67,36 +67,112 @@ void insertionSort(vector<int>& arr, Stats& stats) {
     }
 }
 
+void merge(vector<int>& arr, int left, int mid, int right, Stats& stats) {
+
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1);
+    vector<int> R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+
+        stats.comparisons++;
+
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+
+        stats.swaps++;
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+        stats.swaps++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+        stats.swaps++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right, Stats& stats) {
+
+    if (left < right) {
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid, stats);
+        mergeSort(arr, mid + 1, right, stats);
+
+        merge(arr, left, mid, right, stats);
+    }
+}
+
 int main() {
 
     vector<int> data = generateRandomData(20);
 
-    Stats stats;
+    vector<int> data1 = data;
+    vector<int> data2 = data;
 
-    insertionSort(data, stats);
+    Stats insertionStats;
+    Stats mergeStats;
 
-    cout << "Iterpimo algoritmas" << endl;
-
-    cout << "pries sortiravima: ";
-
-    for (int num : data) {
-        cout << num << " ";
-    }
-
-    cout << endl;
-
-    insertionSort(data, stats);
-
-    cout << "po sortiravimo: ";
+    cout << "Pirmines doumenis:" << endl;
 
     for (int num : data) {
         cout << num << " ";
     }
 
+    cout << endl << endl;
+
+    insertionSort(data1, insertionStats);
+
+    cout << "Iterpimo sorteravimo rezultatai:" << endl;
+
+    for (int num : data1) {
+        cout << num << " ";
+    }
+
+    cout << endl;
+    cout << "Palyginimai: " << insertionStats.comparisons << endl;
+    cout << "Apkeitimai: " << insertionStats.swaps << endl;
+
     cout << endl;
 
-    cout << "paliginimai: " << stats.comparisons << endl;
-    cout << "apkeitimai: " << stats.swaps << endl;
+    mergeSort(data2, 0, data2.size() - 1, mergeStats);
+
+    cout << "Sulejimo sorteravimo rezultatai:" << endl;
+
+    for (int num : data2) {
+        cout << num << " ";
+    }
+
+    cout << endl;
+    cout << "palyginimai: " << mergeStats.comparisons << endl;
+    cout << "Apkeitimai: " << mergeStats.swaps << endl;
 
     return 0;
 }
